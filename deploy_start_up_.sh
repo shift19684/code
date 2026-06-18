1e9538c596854bfd5611c23d27e2250984cec9df
shift19684 <Srikomala_Social_Media@outlook.com>
2026-06-18 15:41:00 +0900

    Add files via upload

--- deploy_start_up_.sh
+++ deploy_start_up_.sh
@@ -6,10 +6,13 @@ repo_url="https://github.com/shift19684/online_startup.git"
 
 target_folder="/home/sudox/online_startup"
 
+save_folder="/home/sudox/startup"
+
+
 git clone "$repo_url" "$target_folder"
 
-sudo cp "${target_folder}/startup.sh" /home/sudox/bangs/startup.sh
+sudo cp "${target_folder}/startup.sh" "${save_folder}/startup.sh"
 
-sudo chmod +x /home/sudox/bangs/startup.sh
+sudo chmod +x "${save_folder}/startup.sh"
 
 sudo rm -rf "$target_folder"
