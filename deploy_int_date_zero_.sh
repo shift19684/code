547f6bb83b2a93990259a0345c5f790bcd316a7d
shift19684 <Srikomala_Social_Media@outlook.com>
2026-06-18 15:11:32 +0900

    Add files via upload

--- deploy_int_date_zero_.sh
+++ deploy_int_date_zero_.sh
@@ -7,3 +7,9 @@ repo_url="https://github.com/shift19684/online_int_date_zero.git"
 target_folder="/home/sudox/online_int_date_zero"
 
 git clone "$repo_url" "$target_folder"
+
+sudo cp /home/sudox/online_int_date_zero/int_date_zero_.sh /home/sudox/bangs/int_date_zero_.sh
+
+sudo chmod +x /home/sudox/bangs/int_date_zero_.sh
+
+sudo rm -rf /home/sudox/online_int_date_zero/
