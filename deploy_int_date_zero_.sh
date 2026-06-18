27ee0365d31f5906483c197f5c5272ca362d489c
shift19684 <Srikomala_Social_Media@outlook.com>
2026-06-18 14:36:59 +0900

    Add files via upload

---
+++ deploy_int_date_zero_.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#deploy int_date_zero
+
+repo_url = "https://github.com/shift19684/online_int_date_zero.git"
+
+target_folder = "/home/sudox/online_int_date_zero/"
+
+git clone $repo_url $target_folder
