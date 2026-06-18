ee7b5e3f63a3cdf3847440fc40ef422975900a1c
shift19684 <Srikomala_Social_Media@outlook.com>
2026-06-18 14:47:38 +0900

    Fix deployment script to create target folder first
    Replaced the original git clone command with a new one after creating the target folder.

--- deploy_int_date_zero_.sh
+++ deploy_int_date_zero_.sh
@@ -6,4 +6,8 @@ repo_url = "https://github.com/shift19684/online_int_date_zero.git"
 
 target_folder = "/home/sudox/online_int_date_zero/"
 
-git clone $repo_url $target_folder
+#git clone $repo_url $target_folder
+
+sudo mkdir $target_folder
+
+git clone "https://github.com/shift19684/online_int_date_zero.git" $target_folder
