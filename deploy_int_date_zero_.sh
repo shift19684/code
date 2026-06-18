65d58d2a55765a28d80cfe31f9d7cb85f03ff0bb
shift19684 <Srikomala_Social_Media@outlook.com>
2026-06-18 15:00:46 +0900

    Fix variable assignment syntax in deploy script
    Removed spaces around assignment operators in the script.

--- deploy_int_date_zero_.sh
+++ deploy_int_date_zero_.sh
@@ -2,8 +2,8 @@
 
 #deploy int_date_zero
 
-repo_url = "https://github.com/shift19684/online_int_date_zero.git"
+repo_url="https://github.com/shift19684/online_int_date_zero.git"
 
-target_folder = "/home/sudox/online_int_date_zero/"
+target_folder="/home/sudox/online_int_date_zero"
 
 git clone "$repo_url" "$target_folder"
