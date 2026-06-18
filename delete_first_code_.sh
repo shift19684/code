2604c043432fc5a3013e21d1e6b4f5781d9ef032
shift19684 <Srikomala_Social_Media@outlook.com>
2026-06-18 16:12:47 +0900

    Add files via upload

--- delete_first_code_.sh
+++ delete_first_code_.sh
@@ -2,7 +2,7 @@
 
 #servo and tvo folder are not deleteed
 #there are venv
-#only the py are deleted
+#the py are replaced
 
 sudo rm -rf /home/sudox/carserv/
 sudo rm -rf /home/sudox/secserv/
@@ -12,5 +12,5 @@ sudo rm -f /home/sudox/servo/serv01/serv2.py
 
 sudo rm -rf /home/sudox/servo/serv01/templates
 
-sudo rm -f /home/sudox/tvo/tv01/tv01.py
+
 
